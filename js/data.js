387ba9cc5229d60e76d2bbf0ee4274085fcbc6ec
Khush Zambare <95687890+KZ283@users.noreply.github.com>
2023-07-24 17:43:25 +0530

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,27 @@
+let movies = [
+  {
+    name: "wanda vision",
+    des: "WandaVision is a captivating Marvel series that blurs the lines between classic television sitcoms and the Marvel Cinematic Universe. The show follows Wanda Maximoff, also known as Scarlet Witch, and Vision, an android, as they navigate through different decades and realities, trying to conceal their true powers while living an idyllic suburban life. However, their blissful existence begins to unravel as they suspect that everything is not as it seems.",
+    image: "assets/img/slider 3.png",
+  },
+  {
+    name: "falcon and the winter soldier",
+    des: "The Falcon and the Winter Soldier is an action-packed Marvel series that continues the legacy of Captain America. Following the events of Avengers: Endgame, Sam Wilson (Falcon) and Bucky Barnes (the Winter Soldier) team up on a global adventure that tests their abilities and their patience. As they confront new threats and grapple with the legacy of Captain America, they must decide if they can work together as a team.",
+    image: "assets/img/slider 2.png",
+  },
+  {
+    name: "raya and the last dragon",
+    des: "Raya and the Last Dragon is a breathtaking animated film set in the fantasy world of Kumandra, where humans and dragons once lived together in harmony. However, when sinister monsters known as the Druun threaten the land, the dragons sacrifice themselves to save humanity. The story follows Raya, a fearless warrior princess, as she embarks on a quest to find the last surviving dragon and save her kingdom from darkness.",
+    image: "assets/img/slider 4.png",
+  },
+  {
+    name: "loki",
+    des: "Loki is a thrilling series based on the beloved Marvel character, Loki, the God of Mischief. After stealing the Tesseract during the events of Avengers: Endgame, Loki finds himself in a mysterious alternate timeline known as the TVA (Time Variance Authority). Forced to answer for his crimes against the sacred timeline, Loki becomes entangled in a time-bending adventure that challenges his identity and alters the destiny of the entire universe.",
+    image: "assets/img/slider 1.png",
+  },
+  {
+    name: "luca",
+    des: "Luca is a heartwarming animated film that transports audiences to the beautiful Italian Riviera. The story follows Luca, a young sea monster, who experiences an unforgettable summer filled with gelato, pasta, and endless scooter rides. However, Luca's secret identity as a sea creature is at risk of being exposed, and he must navigate both the human and underwater worlds while learning the true meaning of friendship and acceptance.",
+    image: "assets/img/slider 5.png",
+  },
+];
